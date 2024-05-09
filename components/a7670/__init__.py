72d5e1536726f119fcc1e6ea1efa82140f51b358
Chad Matsalla <chadmatsalla@gmail.com>
2024-05-09 10:35:43 -0600

    Changes

--- components/a7670/__init__.py
+++ components/a7670/__init__.py
@@ -11,41 +11,41 @@ DEPENDENCIES = ["uart"]
 CODEOWNERS = ["@chino-lu"]
 MULTI_CONF = True
 
-sim7600_ns = cg.esphome_ns.namespace("sim7600")
-Sim7600Component = sim7600_ns.class_("Sim7600Component", cg.Component)
+a7670_ns = cg.esphome_ns.namespace("a7670")
+a7670Component = a7670_ns.class_("a7670Component", cg.Component)
 
-Sim7600ReceivedMessageTrigger = sim7600_ns.class_(
-    "Sim7600ReceivedMessageTrigger",
+a7670ReceivedMessageTrigger = a7670_ns.class_(
+    "a7670ReceivedMessageTrigger",
     automation.Trigger.template(cg.std_string, cg.std_string),
 )
-Sim7600IncomingCallTrigger = sim7600_ns.class_(
-    "Sim7600IncomingCallTrigger",
+a7670IncomingCallTrigger = a7670_ns.class_(
+    "a7670IncomingCallTrigger",
     automation.Trigger.template(cg.std_string),
 )
-Sim7600CallConnectedTrigger = sim7600_ns.class_(
-    "Sim7600CallConnectedTrigger",
+a7670CallConnectedTrigger = a7670_ns.class_(
+    "a7670CallConnectedTrigger",
     automation.Trigger.template(),
 )
-Sim7600CallDisconnectedTrigger = sim7600_ns.class_(
-    "Sim7600CallDisconnectedTrigger",
+a7670CallDisconnectedTrigger = a7670_ns.class_(
+    "a7670CallDisconnectedTrigger",
     automation.Trigger.template(),
 )
 
-Sim7600ReceivedUssdTrigger = sim7600_ns.class_(
-    "Sim7600ReceivedUssdTrigger",
+a7670ReceivedUssdTrigger = a7670_ns.class_(
+    "a7670ReceivedUssdTrigger",
     automation.Trigger.template(cg.std_string),
 )
 
 # Actions
-Sim7600SendSmsAction = sim7600_ns.class_("Sim7600SendSmsAction", automation.Action)
-Sim7600SendUssdAction = sim7600_ns.class_("Sim7600SendUssdAction", automation.Action)
-Sim7600DialAction = sim7600_ns.class_("Sim7600DialAction", automation.Action)
-Sim7600ConnectAction = sim7600_ns.class_("Sim7600ConnectAction", automation.Action)
-Sim7600DisconnectAction = sim7600_ns.class_(
-    "Sim7600DisconnectAction", automation.Action
+a7670SendSmsAction = a7670_ns.class_("a7670SendSmsAction", automation.Action)
+a7670SendUssdAction = a7670_ns.class_("a7670SendUssdAction", automation.Action)
+a7670DialAction = a7670_ns.class_("a7670DialAction", automation.Action)
+a7670ConnectAction = a7670_ns.class_("a7670ConnectAction", automation.Action)
+a7670DisconnectAction = a7670_ns.class_(
+    "a7670DisconnectAction", automation.Action
 )
 
-CONF_SIM7600_ID = "sim7600_id"
+CONF_a7670_ID = "a7670_id"
 CONF_ON_SMS_RECEIVED = "on_sms_received"
 CONF_ON_USSD_RECEIVED = "on_ussd_received"
 CONF_ON_INCOMING_CALL = "on_incoming_call"
@@ -58,39 +58,39 @@ CONF_USSD = "ussd"
 CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
-            cv.GenerateID(): cv.declare_id(Sim7600Component),
+            cv.GenerateID(): cv.declare_id(a7670Component),
             cv.Optional(CONF_ON_SMS_RECEIVED): automation.validate_automation(
                 {
                     cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(
-                        Sim7600ReceivedMessageTrigger
+                        a7670ReceivedMessageTrigger
                     ),
                 }
             ),
             cv.Optional(CONF_ON_INCOMING_CALL): automation.validate_automation(
                 {
                     cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(
-                        Sim7600IncomingCallTrigger
+                        a7670IncomingCallTrigger
                     ),
                 }
             ),
             cv.Optional(CONF_ON_CALL_CONNECTED): automation.validate_automation(
                 {
                     cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(
-                        Sim7600CallConnectedTrigger
+                        a7670CallConnectedTrigger
                     ),
                 }
             ),
             cv.Optional(CONF_ON_CALL_DISCONNECTED): automation.validate_automation(
                 {
                     cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(
-                        Sim7600CallDisconnectedTrigger
+                        a7670CallDisconnectedTrigger
                     ),
                 }
             ),
             cv.Optional(CONF_ON_USSD_RECEIVED): automation.validate_automation(
                 {
                     cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(
-                        Sim7600ReceivedUssdTrigger
+                        a7670ReceivedUssdTrigger
                     ),
                 }
             ),
@@ -100,7 +100,7 @@ CONFIG_SCHEMA = cv.All(
     .extend(uart.UART_DEVICE_SCHEMA)
 )
 FINAL_VALIDATE_SCHEMA = uart.final_validate_device_schema(
-    "sim7600", require_tx=True, require_rx=True
+    "a7670", require_tx=True, require_rx=True
 )
 
 
@@ -129,9 +129,9 @@ async def to_code(config):
         await automation.build_automation(trigger, [(cg.std_string, "ussd")], conf)
 
 
-SIM7600_SEND_SMS_SCHEMA = cv.Schema(
+a7670_SEND_SMS_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.use_id(Sim7600Component),
+        cv.GenerateID(): cv.use_id(a7670Component),
         cv.Required(CONF_RECIPIENT): cv.templatable(cv.string_strict),
         cv.Required(CONF_MESSAGE): cv.templatable(cv.string),
     }
@@ -139,9 +139,9 @@ SIM7600_SEND_SMS_SCHEMA = cv.Schema(
 
 
 @automation.register_action(
-    "sim7600.send_sms", Sim7600SendSmsAction, SIM7600_SEND_SMS_SCHEMA
+    "a7670.send_sms", a7670SendSmsAction, a7670_SEND_SMS_SCHEMA
 )
-async def sim7600_send_sms_to_code(config, action_id, template_arg, args):
+async def a7670_send_sms_to_code(config, action_id, template_arg, args):
     paren = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, paren)
     template_ = await cg.templatable(config[CONF_RECIPIENT], args, cg.std_string)
@@ -151,16 +151,16 @@ async def sim7600_send_sms_to_code(config, action_id, template_arg, args):
     return var
 
 
-SIM7600_DIAL_SCHEMA = cv.Schema(
+a7670_DIAL_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.use_id(Sim7600Component),
+        cv.GenerateID(): cv.use_id(a7670Component),
         cv.Required(CONF_RECIPIENT): cv.templatable(cv.string_strict),
     }
 )
 
 
-@automation.register_action("sim7600.dial", Sim7600DialAction, SIM7600_DIAL_SCHEMA)
-async def sim7600_dial_to_code(config, action_id, template_arg, args):
+@automation.register_action("a7670.dial", a7670DialAction, a7670_DIAL_SCHEMA)
+async def a7670_dial_to_code(config, action_id, template_arg, args):
     paren = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, paren)
     template_ = await cg.templatable(config[CONF_RECIPIENT], args, cg.std_string)
@@ -169,28 +169,28 @@ async def sim7600_dial_to_code(config, action_id, template_arg, args):
 
 
 @automation.register_action(
-    "sim7600.connect",
-    Sim7600ConnectAction,
-    cv.Schema({cv.GenerateID(): cv.use_id(Sim7600Component)}),
+    "a7670.connect",
+    a7670ConnectAction,
+    cv.Schema({cv.GenerateID(): cv.use_id(a7670Component)}),
 )
-async def sim7600_connect_to_code(config, action_id, template_arg, args):
+async def a7670_connect_to_code(config, action_id, template_arg, args):
     paren = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, paren)
     return var
 
 
-SIM7600_SEND_USSD_SCHEMA = cv.Schema(
+a7670_SEND_USSD_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.use_id(Sim7600Component),
+        cv.GenerateID(): cv.use_id(a7670Component),
         cv.Required(CONF_USSD): cv.templatable(cv.string_strict),
     }
 )
 
 
 @automation.register_action(
-    "sim7600.send_ussd", Sim7600SendUssdAction, SIM7600_SEND_USSD_SCHEMA
+    "a7670.send_ussd", a7670SendUssdAction, a7670_SEND_USSD_SCHEMA
 )
-async def sim7600_send_ussd_to_code(config, action_id, template_arg, args):
+async def a7670_send_ussd_to_code(config, action_id, template_arg, args):
     paren = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, paren)
     template_ = await cg.templatable(config[CONF_USSD], args, cg.std_string)
@@ -199,11 +199,11 @@ async def sim7600_send_ussd_to_code(config, action_id, template_arg, args):
 
 
 @automation.register_action(
-    "sim7600.disconnect",
-    Sim7600DisconnectAction,
-    cv.Schema({cv.GenerateID(): cv.use_id(Sim7600Component)}),
+    "a7670.disconnect",
+    a7670DisconnectAction,
+    cv.Schema({cv.GenerateID(): cv.use_id(a7670Component)}),
 )
-async def sim7600_disconnect_to_code(config, action_id, template_arg, args):
+async def a7670_disconnect_to_code(config, action_id, template_arg, args):
     paren = await cg.get_variable(config[CONF_ID])
     var = cg.new_Pvariable(action_id, template_arg, paren)
     return var
