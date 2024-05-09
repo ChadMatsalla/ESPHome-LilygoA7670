03bb78c9bc835de7a4a7034d5609a74964494006
Chad Matsalla <chadmatsalla@gmail.com>
2024-05-09 12:08:38 -0600

    Changes

--- components/a7670_lte/a7670_lte.cpp
+++ components/a7670_lte/a7670_lte.cpp
@@ -7,7 +7,7 @@ namespace a7670_lte {
 static const char *TAG = "a7670_lte.component";
 
 void A7670LTEComponent::setup() {
-  ESP_LOGD("Setting up modem...");
+  ESP_LOGD(TAG, "Setting up modem...");
 }
 
 void A7670LTEComponent::loop() {
@@ -15,11 +15,11 @@ void A7670LTEComponent::loop() {
 }
 
 void A7670LTEComponent::dump_config(){
-    ESP_LOGCONFIG(TAG, "Empty component");
+    ESP_LOGCONFIG(TAG, "A7670Component");
 }
 
 void A7670LTEComponent::publishMessage(std::string topic, std::string message) {
-  ESP_LOGI("Publishing...");
+  ESP_LOGI(TAG, "Publishing...");
 }
 
 
